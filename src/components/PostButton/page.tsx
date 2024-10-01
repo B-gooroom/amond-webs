b851f88adcfa8455063da4ed35b47d51592b194d
sean <j.sean1316@gmail.com>
2024-10-01 18:41:41 +0900

    FIX: 플로팅 버튼 다시 수정..

--- src/components/PostButton/page.tsx
+++ src/components/PostButton/page.tsx
@@ -1,17 +1,15 @@
-import classNames from "classnames";
-
 interface ButtonProps {
   children: React.ReactNode;
 }
 
 export default function PostButton({ children }: ButtonProps) {
   return (
-    <div
-      className={classNames(
-        "fixed z-10 bottom-[95px] w-[100px] h-[48px] right-[16px] bg-ad-brown-800 px-2 py-13 text-subtitle2 text-white flex justify-center items-center rounded-full"
-      )}
-    >
-      {children}
+    <div className="relative">
+      <div className="absolute right-[120px]">
+        <div className="fixed z-20 bottom-[95px] w-[100px] h-[48px] bg-ad-brown-800 px-2 py-13 text-subtitle2 text-white flex justify-center items-center rounded-full">
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
