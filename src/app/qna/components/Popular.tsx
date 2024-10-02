fa2a8ac0db857992fd20f058fbbf9562c339927b
sean <j.sean1316@gmail.com>
2024-10-02 20:37:26 +0900

    STYLE: log 제거

--- src/app/qna/components/Popular.tsx
+++ src/app/qna/components/Popular.tsx
@@ -15,7 +15,6 @@ export default function Popular({
     <div className="flex gap-14 border px-16 py-24 rounded-2xl">
       <div className="text-subtitle1">{index}</div>
       <div className="flex-col gap-8 flex">
-        {/* <p className="text-body2">사업자 등록증 신고할 때 궁금한 점</p> */}
         <p className="text-body2">{title}</p>
         <p className="text-caption1 text-ad-gray-500">
           답변 {comments} ･ 조회수 {views}
