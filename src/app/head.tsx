30a5fb5d738ef7536f1a21adf7de2feecbf5e8a0
sean <j.sean1316@gmail.com>
2024-10-03 21:37:31 +0900

    FIX: metadata (test5)

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,27 +1,23 @@
-export default function Head() {
-  const title = "카페의 모든 것 | 아몬드";
-  const description = "카페의 모든 것을 한눈에 볼 수 있는 곳, 아몬드입니다.";
-  const imageUrl =
-    "https://xtlpqspaohusobjcvsas.supabase.co/storage/v1/object/sign/amond-img/amond-meta.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhbW9uZC1pbWcvYW1vbmQtbWV0YS5qcGciLCJpYXQiOjE3Mjc5NTM2NDEsImV4cCI6MTc3OTc5MzY0MX0.hJ2M_6y66WoTt8A-ci-H5rdTfbgTXkYc2KHm5wdF_nM&t=2024-10-03T11%3A07%3A21.929Z";
-  const siteUrl = "https://amond.kr";
+import { Metadata } from "next";
 
-  return (
-    <>
-      <title>{title}</title>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={imageUrl} />
-      <meta property="og:url" content={siteUrl} />
-      <meta property="og:type" content="website good" />
-      <meta property="og:site_name" content={title} />
-      <meta property="og:locale" content="ko_KR" />
-      <meta
-        name="keywords"
-        content="카페 커뮤니티, 베이커리 커뮤니티, 커피 커뮤니티, 디저트 커뮤니티"
-      />
-    </>
-  );
-}
+export const metadata: Metadata = {
+  title: "카페의 모든 것 | 아몬드",
+  description: "카페의 모든 것을 한눈에 볼 수 있는 곳, 아몬드입니다.",
+  keywords: "카페 커뮤니티, 베이커리 커뮤니티, 커피 커뮤니티, 디저트 커뮤니티",
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: "https://amond.kr/qna",
+    title: "카페의 모든 것 | 아몬드",
+    description: "카페의 모든 것을 한눈에 볼 수 있는 곳, 아몬드입니다.",
+    siteName: "카페의 모든 것 | 아몬드",
+    images: [
+      {
+        url: "https://xtlpqspaohusobjcvsas.supabase.co/storage/v1/object/sign/amond-img/amond-meta.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhbW9uZC1pbWcvYW1vbmQtbWV0YS5qcGciLCJpYXQiOjE3Mjc5NTM2NDEsImV4cCI6MTc3OTc5MzY0MX0.hJ2M_6y66WoTt8A-ci-H5rdTfbgTXkYc2KHm5wdF_nM&t=2024-10-03T11%3A07%3A21.929Z",
+        width: 1200,
+        height: 630,
+        alt: "아몬드 메타 이미지 | Amond Meta Image",
+      },
+    ],
+  },
+};
