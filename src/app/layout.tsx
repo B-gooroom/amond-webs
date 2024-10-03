c77a38936072c0598cd1a0916aa426141f735c70
sean <j.sean1316@gmail.com>
2024-10-03 20:21:33 +0900

    FIX: metadata 형식 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,24 @@
 "use client";
 
 import TabBar from "@/components/TabBar/page";
-import { metadata } from "@/utils/metadata";
-import Head from "next/head";
+// import { metadata } from "@/utils/metadata";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import "./globals.css";
+
+export const metadata = {
+  title: "카페의 모든 것 | 아몬드",
+  description: "카페의 모든 것을 한눈에 볼 수 있는 곳, 아몬드입니다.",
+  keywords: [
+    "카페 커뮤니티",
+    "베이커리 커뮤니티",
+    "커피 커뮤니티",
+    "디저트 커뮤니티",
+  ],
+  imageUrl:
+    "https://xtlpqspaohusobjcvsas.supabase.co/storage/v1/object/sign/amond-img/amond-meta.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhbW9uZC1pbWcvYW1vbmQtbWV0YS5qcGciLCJpYXQiOjE3Mjc5NTM2NDEsImV4cCI6MTc3OTc5MzY0MX0.hJ2M_6y66WoTt8A-ci-H5rdTfbgTXkYc2KHm5wdF_nM&t=2024-10-03T11%3A07%3A21.929Z",
+  siteUrl: "https://amond.kr",
+};
 
 export default function RootLayout({
   children,
@@ -55,7 +68,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <Head>
+      {/* <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta property="og:title" content={metadata.title} />
@@ -64,7 +77,7 @@ export default function RootLayout({
         <meta property="og:url" content={metadata.siteUrl} />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="ko_KR" />
-      </Head>
+      </Head> */}
       <body style={hideTabBar ? { height: "100vh" } : {}}>
         {children}
         <div className="pb-[99px]">
