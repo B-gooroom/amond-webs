ab91c4a8a4d06304e49ff1f3ed2c1e4a321f3577
sean <j.sean1316@gmail.com>
2024-10-03 21:15:56 +0900

    FIX: metadat.ts 추가

---
+++ src/app/metadata.ts
@@ -0,0 +1,23 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "카페의 모든 것 | 아몬드",
+  description: "카페의 모든 것을 한눈에 볼 수 있는 곳, 아몬드입니다.",
+  keywords: "카페 커뮤니티, 베이커리 커뮤니티, 커피 커뮤니티, 디저트 커뮤니티",
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: "https://amond.kr",
+    title: "카페의 모든 것 | 아몬드",
+    description: "카페의 모든 것을 한눈에 볼 수 있는 곳, 아몬드입니다.",
+    siteName: "카페의 모든 것 | 아몬드",
+    images: [
+      {
+        url: "https://xtlpqspaohusobjcvsas.supabase.co/storage/v1/object/sign/amond-img/amond-meta.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhbW9uZC1pbWcvYW1vbmQtbWV0YS5qcGciLCJpYXQiOjE3Mjc5NTM2NDEsImV4cCI6MTc3OTc5MzY0MX0.hJ2M_6y66WoTt8A-ci-H5rdTfbgTXkYc2KHm5wdF_nM&t=2024-10-03T11%3A07%3A21.929Z",
+        width: 1200,
+        height: 630,
+        alt: "아몬드 메타 이미지",
+      },
+    ],
+  },
+};
