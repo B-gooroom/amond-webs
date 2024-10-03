4c2aa9ba1c6ac9f553c112b3f1e70ccf6b5da456
sean <j.sean1316@gmail.com>
2024-10-03 21:29:28 +0900

    FIX: metadata (test2)

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
         url: "https://xtlpqspaohusobjcvsas.supabase.co/storage/v1/object/sign/amond-img/amond-meta.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhbW9uZC1pbWcvYW1vbmQtbWV0YS5qcGciLCJpYXQiOjE3Mjc5NTM2NDEsImV4cCI6MTc3OTc5MzY0MX0.hJ2M_6y66WoTt8A-ci-H5rdTfbgTXkYc2KHm5wdF_nM&t=2024-10-03T11%3A07%3A21.929Z",
         width: 1200,
         height: 630,
-        alt: "아몬드 메타 이미지",
+        alt: "아몬드 메타 이미지 굿굿",
       },
     ],
   },
