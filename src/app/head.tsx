da84ac62ee42430909a41a06e22dd10e31284988
sean <j.sean1316@gmail.com>
2024-10-03 20:59:36 +0900

    FIX: metadata export 수정

---
+++ src/app/head.tsx
@@ -0,0 +1,28 @@
+export default function Head() {
+  const title = "카페의 모든 것 | 아몬드";
+  const description = "카페의 모든 것을 한눈에 볼 수 있는 곳, 아몬드입니다.";
+  const imageUrl =
+    "https://xtlpqspaohusobjcvsas.supabase.co/storage/v1/object/sign/amond-img/amond-meta.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhbW9uZC1pbWcvYW1vbmQtbWV0YS5qcGciLCJpYXQiOjE3Mjc5NTM2NDEsImV4cCI6MTc3OTc5MzY0MX0.hJ2M_6y66WoTt8A-ci-H5rdTfbgTXkYc2KHm5wdF_nM&t=2024-10-03T11%3A07%3A21.929Z";
+  const siteUrl = "https://amond.kr";
+
+  return (
+    <>
+      <title>{title}</title>
+      <meta name="description" content="전역 페이지 설명" />
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content="페이지 설명" />
+      <meta property="og:image" content={imageUrl} />
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="아몬드" />
+      <meta property="og:locale" content="ko_KR" />
+      <meta
+        name="keywords"
+        content="카페 커뮤니티, 베이커리 커뮤니티, 커피 커뮤니티, 디저트 커뮤니티"
+      />
+    </>
+  );
+}
