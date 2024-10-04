3dc0cd46fa1d2ccac03b11af037b0530d05e4475
sean <j.sean1316@gmail.com>
2024-10-04 18:28:40 +0900

    FIX: 프로필 및 프로필 편집 수정

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,9 @@
 import Header from "@/components/Header/page";
+import Label from "@/components/Label/page";
 import PostButton from "@/components/PostButton/page";
 import { Spacer } from "@/components/Spacer/page";
 import { profileUser } from "@/services/profile-user";
+import { getAgeGroup } from "@/utils/ageConverter";
 import { createClient } from "@/utils/supabase/server";
 import Image from "next/image";
 import Link from "next/link";
@@ -13,8 +15,8 @@ export default async function Profile() {
   // console.log("userData", userData);
 
   return (
-    <div className="w-full h-full">
-      <Header title="" rightItems={["setting", "notification"]} />
+    <div className="w-full h-[100vh]">
+      <Header title="소개하다" rightItems={["setting", "notification"]} />
       {userData && (
         <section className="h-[230px]">
           <div className="h-[96px] p-16 flex gap-16">
@@ -26,9 +28,21 @@ export default async function Profile() {
             />
             <div className="flex flex-col gap-8">
               <p className="text-subtitle2">{userData.nickname}</p>
-              <button className="bg-ad-black px-16 py-8 text-ad-white rounded-full text-caption1">
-                프로필 편집
-              </button>
+              <Link
+                href={{
+                  pathname: "/profile/setting",
+                  query: {
+                    nickname: userData.nickname,
+                    introduce: userData.introduce,
+                    age: userData.age,
+                    region: userData.region,
+                  },
+                }}
+              >
+                <Label size="medium" color="black">
+                  프로필 편집
+                </Label>
+              </Link>
             </div>
           </div>
           <div className="px-16 flex flex-col gap-8">
@@ -37,12 +51,12 @@ export default async function Profile() {
             </p>
             <p className="text-body2">{userData.introduce}</p>
             <div className="flex gap-8">
-              <span className="bg-ad-gray-100 px-8 py-[3px] text-caption2 rounded-2xl">
-                {userData.age}세
-              </span>
-              <span className="bg-ad-gray-100 px-8 py-[3px] text-caption2 rounded-full">
+              <Label size="small" color="gray">
+                {getAgeGroup(userData.age)}
+              </Label>
+              <Label size="small" color="gray">
                 {userData.region}
-              </span>
+              </Label>
             </div>
           </div>
           <Spacer className="h-24" />
