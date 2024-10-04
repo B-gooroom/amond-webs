dfe5d9bbe74ab06d81865bacd30a18d19e975142
sean <j.sean1316@gmail.com>
2024-10-04 15:28:06 +0900

    FEAT: 프로필 페이지 추가

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,20 +1,48 @@
 import Header from "@/components/Header/page";
+import PostButton from "@/components/PostButton/page";
+import { Spacer } from "@/components/Spacer/page";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Profile() {
   return (
     <div className="w-full h-full">
-      <Header title="" rightItems={["notification", "notification"]} />
-      <section className="px-16 flex-col flex gap-8 items-center justify-center h-[100vh]">
-        <p>이곳은 프로필입니다.</p>
-        <Image
-          src="/images/ImgHada.png" // public 폴더의 이미지 경로
-          alt="board"
-          width={200}
-          height={200}
-          // sizes="(max-width: 300px) 100vw, 300px"
-        />
+      <Header title="" rightItems={["setting", "notification"]} />
+      <section className="h-[230px]">
+        <div className="h-[96px] p-16 flex gap-16">
+          <Image
+            src="/images/hadaProfile.png"
+            alt="하다프로필이미지"
+            width={64}
+            height={64}
+          />
+          <div className="flex flex-col gap-8">
+            <p className="text-subtitle2">기본 닉네임</p>
+            <button className="bg-ad-black px-16 py-8 text-ad-white rounded-full text-caption1">
+              프로필 편집
+            </button>
+          </div>
+        </div>
+        <div className="px-16 flex flex-col gap-8">
+          <p className="text-caption1">
+            팔로워 {0} | 팔로잉 {0}
+          </p>
+          <p className="text-body2">소개글은 선택, 최대 글자 수는 필수</p>
+          <div className="flex gap-8">
+            <span className="bg-ad-gray-100 px-8 py-[3px] text-caption2 rounded-2xl">
+              20대
+            </span>
+            <span className="bg-ad-gray-100 px-8 py-[3px] text-caption2 rounded-full">
+              서울
+            </span>
+          </div>
+        </div>
+        <Spacer className="h-24" />
+        <p className="px-16 text-body2">나의 활동</p>
       </section>
+      <PostButton>
+        <Link href="/post">+ 글쓰기</Link>
+      </PostButton>
     </div>
   );
 }
