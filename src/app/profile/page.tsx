e1f31e91432dd4e73b7923a178f6da95071c98e0
sean <j.sean1316@gmail.com>
2024-10-04 16:05:06 +0900

    FIX: 프로필 infoCard 추가

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,45 +1,54 @@
 import Header from "@/components/Header/page";
 import PostButton from "@/components/PostButton/page";
 import { Spacer } from "@/components/Spacer/page";
+import { profileUser } from "@/services/profile-user";
+import { createClient } from "@/utils/supabase/server";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Profile() {
+export default async function Profile() {
+  const supabase = createClient();
+  const userData = await profileUser(supabase);
+
+  // console.log("userData", userData);
+
   return (
     <div className="w-full h-full">
       <Header title="" rightItems={["setting", "notification"]} />
-      <section className="h-[230px]">
-        <div className="h-[96px] p-16 flex gap-16">
-          <Image
-            src="/images/hadaProfile.png"
-            alt="하다프로필이미지"
-            width={64}
-            height={64}
-          />
-          <div className="flex flex-col gap-8">
-            <p className="text-subtitle2">기본 닉네임</p>
-            <button className="bg-ad-black px-16 py-8 text-ad-white rounded-full text-caption1">
-              프로필 편집
-            </button>
+      {userData && (
+        <section className="h-[230px]">
+          <div className="h-[96px] p-16 flex gap-16">
+            <Image
+              src="/images/hadaProfile.png"
+              alt="하다프로필이미지"
+              width={64}
+              height={64}
+            />
+            <div className="flex flex-col gap-8">
+              <p className="text-subtitle2">{userData.nickname}</p>
+              <button className="bg-ad-black px-16 py-8 text-ad-white rounded-full text-caption1">
+                프로필 편집
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="px-16 flex flex-col gap-8">
-          <p className="text-caption1">
-            팔로워 {0} | 팔로잉 {0}
-          </p>
-          <p className="text-body2">소개글은 선택, 최대 글자 수는 필수</p>
-          <div className="flex gap-8">
-            <span className="bg-ad-gray-100 px-8 py-[3px] text-caption2 rounded-2xl">
-              20대
-            </span>
-            <span className="bg-ad-gray-100 px-8 py-[3px] text-caption2 rounded-full">
-              서울
-            </span>
+          <div className="px-16 flex flex-col gap-8">
+            <p className="text-caption1">
+              팔로워 {userData.follower} | 팔로잉 {userData.following}
+            </p>
+            <p className="text-body2">{userData.introduce}</p>
+            <div className="flex gap-8">
+              <span className="bg-ad-gray-100 px-8 py-[3px] text-caption2 rounded-2xl">
+                {userData.age}세
+              </span>
+              <span className="bg-ad-gray-100 px-8 py-[3px] text-caption2 rounded-full">
+                {userData.region}
+              </span>
+            </div>
           </div>
-        </div>
-        <Spacer className="h-24" />
-        <p className="px-16 text-body2">나의 활동</p>
-      </section>
+          <Spacer className="h-24" />
+          <p className="px-16 text-body2">나의 활동</p>
+        </section>
+      )}
       <PostButton>
         <Link href="/post">+ 글쓰기</Link>
       </PostButton>
---
+++ src/services/profile-user.tsx
@@ -0,0 +1,21 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+// const { data: { user } } = await supabase.auth.getUser();
+
+export async function profileUser(supabase: SupabaseClient) {
+  const phonenum = "010-1234-5678";
+
+  const { data: userData, error: userError } = await supabase
+    .from("users")
+    .select("*")
+    .in("phonenum", [phonenum])
+    .single();
+
+  if (userError) {
+    console.error("Error fetching user:", userError.message);
+    return [];
+  }
+
+  console.log("userData", userData);
+
+  return userData;
+}
