7195d06782de389d4f4335a1e1a0d503f5a2f4db
sean <j.sean1316@gmail.com>
2024-10-08 18:53:08 +0900

    FEAT: 설정 페이지 퍼블리싱 추가

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -1,42 +1,38 @@
 "use client";
 
+import { User } from "@/app/types/type";
 import Dropdown from "@/components/Dropdown/page";
 import Header from "@/components/Header/page";
 import Icon from "@/components/Icon/page";
 import Input from "@/components/Input/page";
 import { Spacer } from "@/components/Spacer/page";
+import { ProfileUser } from "@/services/profile-user";
+// import ageConverter from "@/utils/ageConverter";
 import { getAgeGroup } from "@/utils/ageConverter";
 import { supabase } from "@/utils/supabase/client";
 import Image from "next/image";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Edit() {
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const [userNickname, setUserNickname] = useState("");
-  const [userAgeGroup, setUserAgeGroup] = useState("");
-  const [userRegion, setUserRegion] = useState("");
-  const [userIntroduce, setUserIntroduce] = useState("");
+  const [userData, setUserData] = useState<User | null>(null);
 
   useEffect(() => {
-    const nickname = searchParams.get("nickname") || "";
-    const age = searchParams.get("age") || "";
-    const region = searchParams.get("region") || "";
-    const introduce = searchParams.get("introduce") || "";
-
-    const getUserData = async () => {
-      const { data, error } = await supabase.auth.getUser();
-      console.log("data", data);
+    const userInfo = async () => {
+      const user = await ProfileUser();
+      if (user) {
+        setUserData(user);
+      }
     };
+    userInfo();
+  }, []);
 
-    getUserData();
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
 
-    setUserNickname(nickname);
-    setUserAgeGroup(getAgeGroup(parseInt(age)));
-    setUserRegion(region);
-    setUserIntroduce(introduce);
-  }, [searchParams]);
+  console.log("userData", userData);
 
   const handleBackClick = () => {
     router.back();
@@ -48,15 +44,18 @@ export default function Edit() {
 
   const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.stopPropagation();
-    setUserNickname(e.target.value);
+    console.log("handleNicknameChange", e.target.value);
+    // setUserNickname(e.target.value);
   };
 
   const handleAgeSelect = (selectedAge: string) => {
-    setUserAgeGroup(selectedAge);
+    // setUserAgeGroup(selectedAge);
+    console.log("selectedAge", selectedAge);
   };
 
   const handleRegionSelect = (selectedRegion: string) => {
-    setUserRegion(selectedRegion);
+    // setUserRegion(selectedRegion);
+    console.log("selectedRegion", selectedRegion);
   };
 
   const handleLogOut = async () => {
@@ -98,68 +97,75 @@ export default function Edit() {
         </div>
       </div>
       <Spacer className="h-24" />
-      <section className="px-16 flex flex-col gap-8">
-        <p className="text-body1">
-          닉네임<span className="text-body1 text-ad-red">*</span>
-        </p>
-        <Input
-          placeholder="닉네임을 설정해주세요"
-          value={userNickname}
-          onChange={handleNicknameChange}
-        />
-      </section>
-      <Spacer className="h-16" />
-      <section className="px-16 flex flex-col gap-8">
-        <p className="text-body1">소개</p>
-        <textarea
-          value={userIntroduce}
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-          onChange={(e) => {
-            e.stopPropagation();
-            setUserIntroduce(e.target.value);
-          }}
-          placeholder="내 관심사나 소개하고 싶은 말을 적어 주세요"
-          className="border text-body2 rounded-2xl px-16 py-12 focus:outline-none focus:ring-2 focus:ring-ad-brown-800 resize-none h-[150px]"
-        ></textarea>
-      </section>
-      <Spacer className="h-16" />
-      <section className="px-16 flex flex-col gap-8">
-        <p className="text-body1">지역</p>
-        <Dropdown
-          defaultValue={userRegion}
-          items={[
-            "서울",
-            "경기",
-            "인천",
-            "강원",
-            "충청",
-            "경상",
-            "전라",
-            "제주",
-          ]}
-          onSelect={handleRegionSelect}
-        />
-      </section>
-      <Spacer className="h-16" />
-      <section className="px-16 flex flex-col gap-8">
-        <p className="text-body1">출생 연도</p>
-        <Dropdown
-          defaultValue={userAgeGroup}
-          items={[
-            "10대",
-            "20대",
-            "30대",
-            "40대",
-            "50대",
-            "60대",
-            "70대",
-            "80대",
-          ]}
-          onSelect={handleAgeSelect}
-        />
-      </section>
+      {userData && (
+        <>
+          <section className="px-16 flex flex-col gap-8">
+            <p className="text-body1">
+              닉네임<span className="text-body1 text-ad-red">*</span>
+            </p>
+            <Input
+              placeholder="닉네임을 설정해주세요"
+              value={userData.nickname}
+              onChange={handleNicknameChange}
+            />
+          </section>
+          <Spacer className="h-16" />
+          <section className="px-16 flex flex-col gap-8">
+            <p className="text-body1">소개</p>
+            <textarea
+              value={userData.introduce}
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+              onChange={(e) => {
+                e.stopPropagation();
+                console.log("소개", e.target.value);
+                // setUserIntroduce(e.target.value);
+              }}
+              placeholder="내 관심사나 소개하고 싶은 말을 적어 주세요"
+              className="border text-body2 rounded-2xl px-16 py-12 focus:outline-none focus:ring-2 focus:ring-ad-brown-800 resize-none h-[150px]"
+            ></textarea>
+          </section>
+          <Spacer className="h-16" />
+          <section className="px-16 flex flex-col gap-8">
+            <p className="text-body1">지역</p>
+            <Dropdown
+              defaultValue={userData.region || "지역을 선택해주세요"}
+              items={[
+                "서울",
+                "경기",
+                "인천",
+                "강원",
+                "충청",
+                "경상",
+                "전라",
+                "제주",
+              ]}
+              onSelect={handleRegionSelect}
+            />
+          </section>
+          <Spacer className="h-16" />
+          <section className="px-16 flex flex-col gap-8">
+            <p className="text-body1">출생 연도</p>
+            <Dropdown
+              defaultValue={
+                getAgeGroup(userData.age) || "출생 연도를 선택해주세요"
+              }
+              items={[
+                "10대",
+                "20대",
+                "30대",
+                "40대",
+                "50대",
+                "60대",
+                "70대",
+                "80대",
+              ]}
+              onSelect={handleAgeSelect}
+            />
+          </section>
+        </>
+      )}
       <div>
         로그아웃
         <button
--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,6 +1,21 @@
 "use client";
 import Header from "@/components/Header/page";
+import Icon from "@/components/Icon/page";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
+
+const settingItems = [
+  { title: "계정", path: "/setting/account" },
+  { title: "북마크", path: "/setting/bookmark" },
+  { title: "좋아요", path: "/setting/like" },
+  { title: "알림 설정", path: "/setting/notification" },
+  { title: "고객센터", path: "/setting/customer-service" },
+  { title: "공지사항", path: "/setting/notice" },
+  { title: "서비스 이용약관", path: "/setting/terms-of-service" },
+  { title: "개인정보처리방침", path: "/setting/privacy-policy" },
+  { title: "차단/신고", path: "/setting/block-report" },
+  { title: "로그아웃", path: "/setting/logout" },
+];
 
 export default function Setting() {
   const router = useRouter();
@@ -14,6 +29,24 @@ export default function Setting() {
       <div onClick={handleBackClick}>
         <Header title="설정" leftItem="IconLeftArrow" />
       </div>
+      <div className="px-16 pt-16">
+        {settingItems.map((item, index) => {
+          console.log(item);
+          const { title, path } = item;
+          return (
+            <Link key={index} href={path}>
+              <p className="py-10 border-b text-body2 text-ad-black">
+                {title}
+                <Icon
+                  icon="IconRightArrow"
+                  className="fill-ad-gray-500 float-right"
+                />
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+      <p className="text-body2 text-ad-gray-500 px-16 pt-10">현재 버전 0.0.1</p>
     </div>
   );
 }
