f1e2c3830309c69dd2f1d3f92fbd22f1b0a13a33
sean <j.sean1316@gmail.com>
2024-10-11 18:17:58 +0900

    FIX: qna images 로직 수정

--- src/components/List/page.tsx
+++ src/components/List/page.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import Label from "../Label/page";
+import { Spacer } from "../Spacer/page";
 
 interface ListProps {
   label: string;
@@ -6,7 +8,7 @@ interface ListProps {
   description: string;
   comments: number;
   views: number;
-  imageUrl?: string;
+  images?: string;
 }
 
 export default function List({
@@ -15,7 +17,7 @@ export default function List({
   description,
   comments,
   views,
-  imageUrl,
+  images,
 }: ListProps) {
   return (
     <div className="flex-col flex gap-8">
@@ -24,23 +26,26 @@ export default function List({
       </Label>
       <p className="text-body2">{title}</p>
       <div className="flex gap-16 w-full border-b pb-12">
-        <div className="flex-col flex">
-          <span className="text-caption1 flex-grow">{description}</span>
+        <div className="flex-col flex max-w-full">
+          <span className="text-caption1 overflow-hidden text-ellipsis line-clamp-2 break-words">
+            {description}
+          </span>
+          <Spacer className="h-8" />
           <span className="text-caption1 flex gap-2 text-ad-gray-500">
             답변 {comments} ･ 조회수 {views}
           </span>
         </div>
-        {imageUrl && (
-          <div className="border rounded-2xl overflow-hidden w-[64px] h-[64px] flex-shrink-0">
-            {/* <Image
-              src={imageUrl}
+        <div className="rounded-2xl overflow-hidden w-[64px] h-[64px] flex-shrink-0">
+          {images && (
+            <Image
+              src={images}
               alt="썸네일 이미지"
               width={64}
               height={64}
               className="object-cover"
-            /> */}
-          </div>
-        )}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
