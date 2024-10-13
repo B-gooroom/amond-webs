9ffd146fa4b15afd6e3dd451818c3ce209d90b48
sean <j.sean1316@gmail.com>
2024-10-13 19:04:40 +0900

    FIX: category type 수정

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,7 +1,7 @@
 export const categories = [
   { id: 1, name: "자유", base: "free" },
   { id: 2, name: "레시피", base: "recipe" },
-  { id: 3, name: "아몬드마켓", base: "market" },
-  { id: 4, name: "매장/점포", base: "shop" },
-  { id: 5, name: "창업", base: "business" },
+  { id: 3, name: "창업", base: "business" },
+  { id: 4, name: "아몬드마켓", base: "market" },
+  { id: 5, name: "매장/점포", base: "shop" },
 ];
