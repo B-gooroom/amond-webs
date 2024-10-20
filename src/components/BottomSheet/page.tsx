93a01f68dd5b9d146323e90e3e40f28801a9d7d1
sean <j.sean1316@gmail.com>
2024-10-21 01:31:14 +0900

    FIX: 바텀시트 수정 및 적용

--- src/components/BottomSheet/page.tsx
+++ src/components/BottomSheet/page.tsx
@@ -1,8 +1,12 @@
+import classNames from "classnames";
+
 interface BottomSheetProps {
   actions: { label: string; onClick: () => void }[];
   isOpen: boolean;
   onClose: () => void; // 닫기 버튼을 추가하기 위한 콜백
 }
+
+// TODO: 버튼 눌리는거랑 바텀시트트 framer motion 추가하기
 
 export function BottomSheet({ actions, isOpen, onClose }: BottomSheetProps) {
   return (
@@ -12,21 +16,26 @@ export function BottomSheet({ actions, isOpen, onClose }: BottomSheetProps) {
       }`}
     >
       <div
-        className="absolute inset-0 bg-gray-900 bg-opacity-50"
+        className="absolute inset-0 bg-gray-900 bg-opacity-30"
         onClick={onClose} // 바깥 클릭 시 닫힘
       ></div>
 
       <div
-        className={`fixed inset-x-0 bottom-0 bg-white p-4 rounded-t-lg shadow-lg transform transition-transform duration-300 ease-in-out ${
+        className={`fixed inset-x-0 bottom-0 bg-white px-16 pt-24 rounded-t-2xl shadow-lg transform transition-transform duration-300 ease-in-out ${
           isOpen ? "translate-y-0" : "translate-y-full"
         }`}
-        style={{ height: "148px" }}
+        style={{
+          height: "148px",
+        }}
       >
         <ul>
           {actions.map((action, index) => (
-            <li key={index}>
+            <li key={index} className="p-10">
               <button
-                className="w-full text-left p-10 text-body1"
+                className={
+                  (classNames("w-full text-left text-body1"),
+                  action.label === "신고하기" ? "text-ad-red" : "text-ad-black")
+                }
                 onClick={action.onClick}
               >
                 {action.label}
--- src/components/UserInfoDetail/page.tsx
+++ src/components/UserInfoDetail/page.tsx
@@ -1,5 +1,7 @@
 import { formatDate } from "@/utils/formateDate";
 import Image from "next/image";
+import { useState } from "react";
+import { BottomSheet } from "../BottomSheet/page";
 import Icon from "../Icon/page";
 import { Spacer } from "../Spacer/page";
 
@@ -7,13 +9,32 @@ interface UserInfoDetailProps {
   profile_image?: string;
   userNickname: string;
   created_at: Date;
+  isWriter: boolean;
 }
 
 export default function UserInfoDetail({
   profile_image,
   userNickname,
   created_at,
+  isWriter,
 }: UserInfoDetailProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleEdit = () => {
+    console.log("글 수정하기 클릭");
+  };
+
+  const handleDelete = () => {
+    console.log("글 삭제하기 클릭");
+  };
+
+  const handleOpenBottomSheet = () => {
+    setIsOpen(true);
+  };
+  const handleCloseBottomSheet = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="flex gap-16">
       {profile_image ? (
@@ -39,8 +60,33 @@ export default function UserInfoDetail({
             {formatDate(created_at)}
           </p>
         </div>
-        <Icon icon="IconMore" size={24} />
+        <div onClick={handleOpenBottomSheet}>
+          <Icon icon="IconMore" size={24} />
+        </div>
       </div>
+      {isOpen &&
+        (isWriter ? (
+          <BottomSheet
+            isOpen={isOpen}
+            onClose={handleCloseBottomSheet}
+            actions={[
+              { label: "글 수정하기", onClick: handleEdit },
+              { label: "글 삭제하기", onClick: handleDelete },
+            ]}
+          />
+        ) : (
+          <BottomSheet
+            isOpen={isOpen}
+            onClose={handleCloseBottomSheet}
+            actions={[
+              { label: "이 사용자의 글 보지 않기", onClick: handleEdit },
+              {
+                label: "신고하기",
+                onClick: () => console.log("신고하기 클릭"),
+              },
+            ]}
+          />
+        ))}
     </div>
   );
 }
