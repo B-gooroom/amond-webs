e9c52176a881cccb7c9fa5355435a1dd6d1cf8ed
sean <j.sean1316@gmail.com>
2024-10-20 18:57:50 +0900

    FIX: Input, Label 컴포넌트 수정

--- src/components/Input/page.tsx
+++ src/components/Input/page.tsx
@@ -1,22 +1,23 @@
-// components/Input.tsx
 import React from "react";
 
 interface InputProps {
   label?: string;
   placeholder?: string;
   value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   type?: string;
   disabled?: boolean;
+  className?: string; // className을 전달받기 위해 추가
 }
 
 const Input: React.FC<InputProps> = ({
   label,
   placeholder,
-  value,
+  value = "",
   onChange,
   type = "text",
   disabled = false,
+  className,
 }) => {
   const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
     e.stopPropagation(); // 이벤트 전파 방지
@@ -32,7 +33,7 @@ const Input: React.FC<InputProps> = ({
         placeholder={placeholder}
         disabled={disabled}
         onClick={handleClick}
-        className="border text-body2 px-16 py-10 rounded-2xl focus:outline-none focus:ring-2 focus:ring-ad-brown-800"
+        className={`border text-body2 px-16 py-10 rounded-2xl focus:outline-none focus:ring-2 focus:ring-ad-brown-800 ${className}`}
       />
     </div>
   );
--- src/components/Label/page.tsx
+++ src/components/Label/page.tsx
@@ -10,40 +10,36 @@ export default function Label({ children, size, color }: LabelProps) {
   const isDarkBackground = color === "brown" || color === "black";
 
   return size === "small" ? (
-    <div>
-      <span
-        className={classnames(
-          "text-caption2 rounded-2xl px-8 py-4",
-          color === "brown"
-            ? "bg-ad-brown-800"
-            : color === "gray"
-            ? "bg-ad-gray-100"
-            : color === "black"
-            ? "bg-ad-black"
-            : "border",
-          isDarkBackground ? "text-white" : "text-black"
-        )}
-      >
-        {children}
-      </span>
+    <div
+      className={classnames(
+        "text-caption2 rounded-2xl px-8 py-4",
+        color === "brown"
+          ? "bg-ad-brown-800"
+          : color === "gray"
+          ? "bg-ad-gray-100"
+          : color === "black"
+          ? "bg-ad-black"
+          : "border",
+        isDarkBackground ? "text-white" : "text-black"
+      )}
+    >
+      {children}
     </div>
   ) : (
-    <div>
-      <span
-        className={classnames(
-          "text-caption1 rounded-2xl px-16 py-8",
-          color === "brown"
-            ? "bg-ad-brown-800"
-            : color === "gray"
-            ? "bg-ad-gray-100"
-            : color === "black"
-            ? "bg-ad-black"
-            : "border",
-          isDarkBackground ? "text-white" : "text-black"
-        )}
-      >
-        {children}
-      </span>
+    <div
+      className={classnames(
+        "text-caption1 rounded-2xl px-16 py-8",
+        color === "brown"
+          ? "bg-ad-brown-800"
+          : color === "gray"
+          ? "bg-ad-gray-100"
+          : color === "black"
+          ? "bg-ad-black"
+          : "border",
+        isDarkBackground ? "text-white" : "text-black"
+      )}
+    >
+      {children}
     </div>
   );
 }
