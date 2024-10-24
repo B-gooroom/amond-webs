d507b10711be12e1d5308d3a09d0751850fc138a
sean <j.sean1316@gmail.com>
2024-10-24 15:53:06 +0900

    FIX: qna - 상세 수정3

--- src/app/qna/[id]/page.tsx
+++ src/app/qna/[id]/page.tsx
@@ -150,12 +150,12 @@ export default function QnaDetailPage() {
             )}
             <Spacer className="h-16" />
             <div className="pb-[32px] border-b flex gap-16">
-              <div onClick={() => addLike({ qna_id })}>
+              <div onClick={() => addLike({ qna_id })} className="w-fit">
                 <Icon
                   icon={hasLiked ? "IconFavoriteActive" : "IconFavorite"}
                   size={24}
                 />
-                <span className="text-caption1 w-[20px]">{likesCount}</span>
+                <span className="text-caption1">{likesCount}</span>
               </div>
               <div onClick={() => addBookmark({ qna_id })}>
                 <Icon
